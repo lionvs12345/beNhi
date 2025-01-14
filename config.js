add4c9c0a8a14be4b5111307777628f535f3f5df
teq-kietdo <kietdo@teqnological.asia>
2025-01-14 19:09:43 +0700

    first

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bé Mỳ ơiiiiiiiiiii!',
+    introDesc: `Bé iu có thương anh hơmmmmmmmmmm`,
+    btnIntro: 'hihi',
+    title: 'Chúc mừng sinh nhật bé iu của anh nè',
+    desc: 'Bé có thấy anh đẹp troai honggggggg',
+    btnYes: 'Siêu đập troai <3',
+    btnNo: 'Hông :3',
+    question:'Chúc bé yêu của anh sinh nhật vui vẻ, tràn đầy hạnh phúc và nhất là phải thương anh thiệt thiệt nhiều, thương suốt đời luôn nha',
+    btnReply: 'Moahhhhhhhhhhhhhhhh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
